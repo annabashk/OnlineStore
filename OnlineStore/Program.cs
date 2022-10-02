80b3633e88ef9d200dfc94b82b08dc180ba99c04
Анна Башкатова <annabashk13@gmail.com>
2022-10-02 21:08:00 +0300

    начало

--- OnlineStore/Program.cs
+++ OnlineStore/Program.cs
@@ -1,2 +1,27 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿abstract class Delivery
+{
+    public string Address; 
+}
+
+class HomeDelivery : Delivery
+{
+
+}
+class PickPointDelivery : Delivery
+{
+
+}
+class ShopDelivery : Delivery
+{
+
+}
+class Order <TDelivery, TStruct> where TDelivery : Delivery
+{
+    public TDelivery Delivery;
+    public int Number;
+    public string Description;
+    public void DisplayAddress()
+    {
+        Console.WriteLine(Delivery.Address);
+    }
+}
