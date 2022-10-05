2a6cd7e968ce51a5b01a5ecba72ed1644d28108f
Анна Башкатова <annabashk13@gmail.com>
2022-10-05 21:00:12 +0300

    добавлен класс для оплаты и для описания заказа

--- OnlineStore/Program.cs
+++ OnlineStore/Program.cs
@@ -1,19 +1,91 @@
-﻿abstract class Delivery
+﻿/// <summary>
+/// Адрес
+/// </summary>
+abstract class Adress
 {
-    public string Address; 
+    private string _sity;
+    private string _street;
+    private int _home;
+
+    public string Sity
+    { get { return _sity; } set { _sity = value; } }
+    public string Street
+    { get { return _street; } set { _street = value; } }
+    public int Home
+    { get { return _home; } 
+        set 
+        { 
+            if (_home > 0)
+            {
+                _home = value;
+            }
+            else { Console.WriteLine("Номер дома не может быть отрицательным числом"); }
+        } }
 }
 
+/// <summary>
+/// Доставка
+/// </summary>
+abstract class Delivery : Adress
+{
+    
+}
+
+/// <summary>
+/// Доставка на дом
+/// </summary>
 class HomeDelivery : Delivery
 {
 
 }
+
+/// <summary>
+/// Доставка в пункт выдачи
+/// </summary>
 class PickPointDelivery : Delivery
 {
 
 }
+
+/// <summary>
+/// Доставка в розничный магазин
+/// </summary>
 class ShopDelivery : Delivery
 {
 
+}
+
+/// <summary>
+/// Оплата
+/// </summary>
+abstract class Payment
+{
+
+}
+
+/// <summary>
+/// Оптата наличными
+/// </summary>
+class CashPayment : Payment
+{
+
+}
+
+/// <summary>
+/// Оплата картой
+/// </summary>
+class CardPayment : Payment
+{
+
+}
+
+/// <summary>
+/// Продукт
+/// </summary>
+class Product
+{
+    public string Name;
+    public int Art;
 }
 class Order <TDelivery, TStruct> where TDelivery : Delivery
 {
@@ -22,6 +94,6 @@ class Order <TDelivery, TStruct> where TDelivery : Delivery
     public string Description;
     public void DisplayAddress()
     {
-        Console.WriteLine(Delivery.Address);
+        Console.WriteLine();
     }
 }
